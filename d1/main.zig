32b27a464dd80a29c4f63a7ff5c00f2d3259da1c
Nátán Sandle <natansandle@gmail.com>
2025-12-10 23:26:05 +0100

    d1 part2

--- d1/main.zig
+++ d1/main.zig
@@ -8,34 +8,44 @@ pub fn main() !void {
     var freader = file.reader(&buffer);
     const reader = &freader.interface;
 
+    var landed_zero_count: u32 = 0;
+    var passed_zero_count: u32 = 0;
+
+    var pos: u32 = 50;
+
     var line_no: usize = 0;
-    var zero_count: u16 = 0;
+    while (reader.takeDelimiter('\n')) |maybe_line| {
+        const line = maybe_line orelse break;
 
-    var pos: u8 = 50;
-
-    while (reader.takeDelimiterExclusive('\n')) |line| {
-        defer reader.toss(1);
         line_no += 1;
-        if (line.len > 0) {
-            const direction = line[0];
-            const distance_str = line[1..];
-
-            const distance: u8 = @intCast((try std.fmt.parseInt(u16, distance_str, 10)) % 100);
-
-            switch (direction) {
-                'L' => pos = (pos + 100 - distance) % 100,
-                'R' => pos = (pos + distance) % 100,
-                else => unreachable,
-            }
-
-            if (pos == 0) {
-                zero_count += 1;
-            }
+        if (line.len < 2) {
+            unreachable;
         }
-    } else |err| switch (err) {
-        error.EndOfStream => {},
-        else => return err,
-    }
+        const direction = line[0];
+        const distance_str = line[1..];
 
-    std.debug.print("{d}\n", .{zero_count});
+        const distance: u32 = try std.fmt.parseInt(u32, distance_str, 10);
+
+        passed_zero_count += distance / 100;
+        const remainder: u32 = distance % 100;
+
+        switch (direction) {
+            'L' => {
+                passed_zero_count += if (remainder >= pos and pos != 0) 1 else 0;
+                pos = (pos + 100 - remainder) % 100;
+            },
+            'R' => {
+                passed_zero_count += if (remainder >= 100 - pos) 1 else 0;
+                pos = (pos + remainder) % 100;
+            },
+            else => unreachable,
+        }
+
+        if (pos == 0) {
+            landed_zero_count += 1;
+        }
+    } else |err| return err;
+
+    std.debug.print("Landed on zero {d} times\n", .{landed_zero_count});
+    std.debug.print("Passed zero {d} times\n", .{passed_zero_count});
 }
