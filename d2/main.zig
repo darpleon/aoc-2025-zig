4efeabdbdb84a312eaab894fa63a322a817bcddd
Nátán Sandle <natansandle@gmail.com>
2025-12-24 22:04:21 +0100

    d2 part2 (some nasty stuff in there but it works)

--- d2/main.zig
+++ d2/main.zig
@@ -1,15 +1,14 @@
 const std = @import("std");
-// Get the ends of the ranges as strings
-// Clamp if odd digit count
-// Split the strings in half and parse as ints
 
 pub fn splitByHyphen(range: []const u8) !struct { []const u8, []const u8 } {
-    const dash_index: usize = std.mem.indexOfScalar(u8, range, '-') orelse return error.NoHyphen;
+    const dash_index: usize = std.mem.indexOfScalar(u8, range, '-') orelse return error.MissingHyphen;
     return .{ range[0..dash_index], range[dash_index + 1 ..] };
 }
 
 const LeftRight = struct { left: u64, right: u64 };
 const StartEnd = struct { start: LeftRight, end: LeftRight };
+
+const NumPow = struct { num: u64, pow: u64 };
 
 pub fn pow10(exponent: u64) u64 {
     return std.math.powi(u64, 10, exponent) catch unreachable;
@@ -17,6 +16,48 @@ pub fn pow10(exponent: u64) u64 {
 
 pub fn sumBetween(a: u64, b: u64) u64 {
     return (b + a) * (b - a + 1) / 2;
+}
+
+pub fn splitNum(allocator: std.mem.Allocator, n: u64, num_str: []const u8) ![]const u64 {
+    std.debug.assert(num_str.len % n == 0);
+    const chunk_size = num_str.len / n;
+    var parts = try std.ArrayList(u64).initCapacity(allocator, n);
+    errdefer parts.deinit(allocator);
+    for (0..n) |i| {
+        const chunk = num_str[i * chunk_size .. (i + 1) * chunk_size];
+        parts.appendAssumeCapacity(try std.fmt.parseInt(u64, chunk, 10));
+    }
+    return parts.toOwnedSlice(allocator);
+}
+
+pub fn lowestBlock(allocator: std.mem.Allocator, n: u64, num_str: []const u8) !u64 {
+    if (num_str.len % n == 0) {
+        const split: []const u64 = try splitNum(allocator, n, num_str);
+        const first = split[0];
+        for (split[1..]) |num| {
+            if (num < first) return first;
+            if (num > first) return first + 1;
+        }
+        return first;
+    } else {
+        const pow = num_str.len / n;
+        return std.math.powi(u64, 10, pow);
+    }
+}
+
+pub fn highestBlock(allocator: std.mem.Allocator, n: u64, num_str: []const u8) !u64 {
+    if (num_str.len % n == 0) {
+        const split: []const u64 = try splitNum(allocator, n, num_str);
+        const first = split[0];
+        for (split[1..]) |num| {
+            if (num > first) return first;
+            if (num < first) return first - 1;
+        }
+        return first;
+    } else {
+        const pow = num_str.len / n;
+        return (try std.math.powi(u64, 10, pow)) - 1;
+    }
 }
 
 pub fn main() !void {
@@ -32,55 +73,40 @@ pub fn main() !void {
 
     file.close();
 
+    var fixed_buf: [16384]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&fixed_buf);
+    const allocator = fba.allocator();
+
     const trimmed = std.mem.trimEnd(u8, content[0..size], "\n");
 
     var sum: u64 = 0;
 
     var it = std.mem.splitScalar(u8, trimmed, ',');
     while (it.next()) |range_str| {
+        defer fba.reset();
         const start_str, const end_str = try splitByHyphen(range_str);
 
-        const start_width: u64 = @intCast(start_str.len / 2);
-        const end_width: u64 = @intCast(end_str.len / 2);
+        var found = std.AutoArrayHashMap(u64, void).init(allocator);
+        for (2..@max(start_str.len, end_str.len) + 1) |n| {
+            const lowest: u64 = try lowestBlock(allocator, n, start_str);
+            const highest: u64 = try highestBlock(allocator, n, end_str);
 
-        const start: LeftRight = if (start_str.len % 2 == 0) .{
-            .left = try std.fmt.parseInt(u64, start_str[0 .. start_str.len / 2], 10),
-            .right = try std.fmt.parseInt(u64, start_str[start_str.len / 2 ..], 10),
-        } else blk: {
-            const smallest = try std.math.powi(u64, 10, start_width);
-            break :blk .{ .left = smallest, .right = smallest };
-        };
-        const end: LeftRight = if (end_str.len % 2 == 0) .{
-            .left = try std.fmt.parseInt(u64, end_str[0 .. end_str.len / 2], 10),
-            .right = try std.fmt.parseInt(u64, end_str[end_str.len / 2 ..], 10),
-        } else blk: {
-            const largest = try std.math.powi(u64, 10, end_width) - 1;
-            break :blk .{ .left = largest, .right = largest };
-        };
+            if (lowest > highest) continue;
 
-        const first: u64 = if (start.left >= start.right) start.left else start.left + 1;
-        const last: u64 = if (end.left <= end.right) end.left else end.left - 1;
-
-        if (last < first) continue;
-
-        const start_pow = std.math.log10_int(start.left);
-        const end_pow = std.math.log10_int(end.left);
-
-        const start_ten: u64 = pow10(start_pow + 1);
-        if (end_pow == start_pow) {
-            const gauss = sumBetween(first, last);
-            sum += (start_ten + 1) * gauss;
-            continue;
+            var base: u64 = std.math.log10_int(lowest);
+            for (lowest..highest + 1) |num| {
+                if (num >= try std.math.powi(u64, 10, base)) base += 1;
+                var entry: u64 = 0;
+                for (0..n) |i| {
+                    entry += num * try std.math.powi(u64, 10, i * base);
+                }
+                try found.put(entry, {});
+            }
         }
-
-        const start_gauss = sumBetween(first, start_ten - 1);
-        const end_gauss = sumBetween(start_ten, last);
-        sum += (start_ten + 1) * start_gauss + (start_ten * 10 + 1) * end_gauss;
-        if (end_pow - start_pow == 1) continue;
-
-        sum += 55 * (pow10(3 * end_pow) - pow10(3 * start_pow)) / 111;
-        sum -= 5 * (pow10(2 * end_pow) - pow10(2 * start_pow)) / 11;
-        sum += sumBetween(start_ten, pow10(end_pow));
+        for (found.keys()) |num| {
+            sum += num;
+        }
+        found.clearAndFree();
     }
 
     std.debug.print("sum: {d}\n", .{sum});
