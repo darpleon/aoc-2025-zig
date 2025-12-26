737e3a2b54c2cace4c1bf29b1865efd3deba93eb
Nátán Sandle <natansandle@gmail.com>
2025-12-26 14:50:28 +0100

    d3 part2

--- d3/main.zig
+++ d3/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const digit_count = 12;
+
 pub fn main() !void {
     const file = try std.fs.cwd().openFile("input", .{});
     defer file.close();
@@ -13,21 +15,26 @@ pub fn main() !void {
         const line = maybe_line orelse break;
         if (line.len < 2) return error.LineTooShort;
 
-        var left_candidate: u64 = 0;
-        var right_candidate: u64 = 0;
+        var digits: [digit_count]u64 = .{0} ** digit_count;
         for (line) |char| {
-            const digit: u64 = char - '0';
+            const new_digit: u64 = char - '0';
 
-            if (left_candidate < right_candidate) {
-                left_candidate = right_candidate;
-                right_candidate = 0;
+            for (0..digit_count - 1) |i| {
+                if (digits[i] < digits[i + 1]) {
+                    digits[i] = digits[i + 1];
+                    digits[i + 1] = 0;
+                }
             }
-
-            if (digit > right_candidate) {
-                right_candidate = digit;
+            
+            if (new_digit > digits[digit_count - 1]) {
+                digits[digit_count - 1] = new_digit;
             }
         }
-        sum += left_candidate * 10 + right_candidate;
+        var pow10: u64 = 1;
+        for (0..digit_count) |i| {
+            sum += pow10 * digits[digit_count - i - 1];
+            pow10 *= 10;
+        }
     } else |err| return err;
     std.debug.print("sum: {d}\n", .{sum});
 }
